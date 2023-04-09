33f796c2a611c2c039eb842bba40e57ac1f09c6c
billchetch <45006134+billchetch@users.noreply.github.com>
2023-04-09 23:25:29 +0800

    s

--- examples/adm_ethernet/adm_ethernet.ino
+++ examples/adm_ethernet/adm_ethernet.ino
@@ -3,13 +3,18 @@
 #include <ChetchADMConfig.h>
 #include <ChetchADM.h>
 
+/*
+* NOTE: If you encounter problems using a standalone power supply (i.e. something other than the USB connector to a computer)
+* this may well be due to a 'bad' power supply.  Try different power supplies to ensure you get a smooth DC current and a fixed voltage.
+*/
 
-#define TRACE true
+#define TRACE true 
 
 #define HOSTNAME "crayfish9"
 #define PORT 8091
-#define NETWORK_SERVICE_SERVER_IP "192.168.2.180"
+#define NETWORK_SERVICE_SERVER_IP "192.168.4.188"
 #define NETWORK_SERVICE_SERVER_PORT 8001
+#define NETWORK_SERVICE_CONNECT_TIMEOUT 10000
 
 //SFC values (these values are for Mega)
 #define LOCAL_UART_BUFFER 256
@@ -18,9 +23,11 @@
 #define SEND_BUFFER 2*LOCAL_UART_BUFFER
 #define CTS_TIMEOUT 1000
 
+//change values below to fit your network
+//NOTE: the Ethernet ENC 28J60works best with a fixed IP
 byte mac[] = {  0x00, 0xAD, 0xBE, 0xEF, 0xFE, 0xEF };
-byte ip[] = { 192, 168, 2, 10 };    
-byte router[] = { 192, 168, 2, 2 };
+byte ip[] = { 192, 168, 4, 10 };    
+byte router[] = { 192, 168, 4, 1};
 byte subnet[] = { 255, 255, 255, 0 };
 
 EthernetServer server(PORT);
@@ -34,11 +41,14 @@ ArduinoDeviceManager* ADM;
 void setup() {
   Serial.begin(115200);
 
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
   EthernetManager::trace = TRACE;
   NetworkAPI::trace = TRACE;
 
   if(EthernetManager::begin(mac, ip, router, subnet)){
-    int statusCode = NetworkAPI::registerService(client, NETWORK_SERVICE_SERVER_IP, NETWORK_SERVICE_SERVER_PORT, HOSTNAME, PORT);
+    int statusCode = NetworkAPI::registerService(client, NETWORK_SERVICE_SERVER_IP, NETWORK_SERVICE_SERVER_PORT, HOSTNAME, PORT, NETWORK_SERVICE_CONNECT_TIMEOUT);
     if(TRACE){
       Serial.println(statusCode == 200 ? "Successfully registered service" : "Failed to register service");
     }
